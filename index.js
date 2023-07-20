e36cc08c83dc0e71613c106c609bf997dce3002d
Aryan192003 <aryan831942@gmail.com>
2023-07-20 15:48:41 +0530

    setting static files acess for layouts

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const layout = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(layout);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes/inedx'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
--- routes/inedx.js
+++ routes/inedx.js
@@ -6,6 +6,8 @@ const homecont = require('../controllers/home_controller');
 console.log('everything is fine');
 
 router.get('/', homecont.home);
+router.use('/users', require('./users') );
+router.use('/post', require('./post'));
 
 
 module.exports = router;
